135a228367fbe9af2ffbfc226b66b3baa3242eb1
aaronr5 <aaronr222@hotmail.com>
2017-03-26 23:03:53 -0400

    edited config

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ exports.TEST_DATABASE_URL = (
 exports.PORT = process.env.PORT || 8080;
 
 //JWT secret and expiration time//
-exports.SECRET = process.env.SECRET;
-exports.EXPIRATIONTIME = process.env.EXPIRATIONTIME;
+exports.SECRET = process.env.SECRET || 'supersecretlullullul';
+exports.EXPIRATIONTIME = process.env.EXPIRATIONTIME || 60*60*24;
 
 //AWS s3 configuration keys//
-exports.AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-exports.AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-exports.AWS_BUCKET = process.env.AWS_BUCKET;
+exports.AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY || 'AKIAJPEDYIGKCR7DABXQ';
+exports.AWS_SECRET_KEY = process.env.AWS_SECRET_KEY || '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';
+exports.AWS_BUCKET = process.env.AWS_BUCKET || 'mxs-db-uploads';
