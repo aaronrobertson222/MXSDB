701d6826ddde168424efc1cf1f95ee2d6065f18a
aaronr5 <aaronr222@hotmail.com>
2017-10-11 16:49:28 -0400

    initial react setup, webpack config added, and added eslint.

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Porter</h1>, document.getElementById('app'));
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,96 +1,89 @@
-const {BasicStrategy} = require('passport-http');
+const { BasicStrategy } = require('passport-http');
 const express = require('express');
 const passport = require('passport');
-const {User} = require('./userModel');
+const { User } = require('./userModel');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
-//POST route to create a new user
+// POST route to create a new user
 router.post('/', (req, res) => {
   if (!req.body) {
-    return res.status(400).json({message: 'No request body'});
+    return res.status(400).json({ message: 'No request body' });
   }
 
   if (!('username' in req.body)) {
-    return res.status(422).json({message: 'Missing field: username'});
+    return res.status(422).json({ message: 'Missing field: username' });
   }
 
-  let {username, password, firstName, lastName} = req.body;
+  let {
+    username, password, firstName, lastName,
+  } = req.body;
 
 
   if (typeof username !== 'string') {
-    return res.status(422).json({message: 'Incorrect field type: username'});
+    return res.status(422).json({ message: 'Incorrect field type: username' });
   }
 
   username = username.trim();
 
   if (username === '') {
-    return res.status(422).json({message: 'Incorrect field length: username'});
+    return res.status(422).json({ message: 'Incorrect field length: username' });
   }
 
   if (!(password)) {
-    return res.status(422).json({message: 'Missing field: password'});
+    return res.status(422).json({ message: 'Missing field: password' });
   }
 
   if (typeof password !== 'string') {
-    return res.status(422).json({message: 'Incorrect field type: password'});
+    return res.status(422).json({ message: 'Incorrect field type: password' });
   }
 
   password = password.trim();
 
   if (password === '') {
-    return res.status(422).json({message: 'Incorrect field length: password'});
+    return res.status(422).json({ message: 'Incorrect field length: password' });
   }
 
   return User
-    .find({username})
+    .find({ username })
     .count()
     .exec()
-    .then(count => {
+    .then((count) => {
       if (count > 0) {
-        return res.status(422).json({message: 'Username already taken.'});
+        return res.status(422).json({ message: 'Username already taken.' });
       }
-      return User.hashPassword(password)
+      return User.hashPassword(password);
     })
-    .then(hash => {
-      return User
-        .create({
-          username: username,
-          password: hash,
-          firstName: firstName,
-          lastName: lastName,
-          joinedDate: Date.now()
-        })
-    })
-    .then(user => {
-      return res.status(201).json(user.apiRepr());
-    })
-    .catch(err => {
-      return res.status(500).sendFile(path.join(__dirname, '../public', 'error.html'))
-    });
+    .then(hash => User
+      .create({
+        username,
+        password: hash,
+        firstName,
+        lastName,
+        joinedDate: Date.now(),
+      }))
+    .then(user => res.status(201).json(user.apiRepr()))
+    .catch(err => res.status(500).sendFile(path.join(__dirname, '../public', 'error.html')));
 });
 
-router.get('/downloadCount',
-    passport.authenticate('jwt', {session: false}), (req, res) => {
-      return Users
-              .findOneAndUpdate({'_id': req.user._id}, {$inc: {'uploads': 1}})
-              .exec()
-              .then(item => {
-                return res.status(200).json({message: 'success'});
-              })
-              .catch(err => {
-                return console.log(err);
-              });
-
-    });
+router.get(
+  '/downloadCount',
+  passport.authenticate('jwt', { session: false }), (req, res) => Users
+    .findOneAndUpdate({ _id: req.user._id }, { $inc: { uploads: 1 } })
+    .exec()
+    .then(item => res.status(200).json({ message: 'success' }))
+    .catch(err => console.log(err)),
+);
 
 require('../passport')(passport);
 
 // route used to verify user has valid JWT
-router.get('/me',
-  passport.authenticate('jwt', {session: false}), (req, res, user) => {
-    res.status(200).json({user: req.user.apiRepr()});
-  });
+router.get(
+  '/me',
+  passport.authenticate('jwt', { session: false }), (req, res, user) => {
+    res.status(200).json({ user: req.user.apiRepr() });
+  },
+);
 
-module.exports = {router};
+module.exports = { router };
