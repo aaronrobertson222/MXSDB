c24927a93cda931dcbd4fedb8bc747d3f2cb2917
aaronr5 <aaronr222@hotmail.com>
2017-10-16 16:41:03 -0400

    finished react/redux setup

--- src/containers/app/app.jsx
+++ src/containers/app/app.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import Navbar from 'components/navbar/navbar';
+
 const App = () => (
-  <h1>Porter</h1>
+  <Router>
+    <Navbar />
+  </Router>
 );
 
 export default connect()(App);
