24255394196f69eefbca4ce2e68adbeea9a148e8
aaronr5 <aaronr222@hotmail.com>
2017-10-17 20:55:52 -0400

    added navbar component and added glamorous dependency

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import glamorous from 'glamorous';
+
+const Header = glamorous.header({
+  width: '100%',
+});
+
+const Nav = glamorous.nav({
+  width: '100%',
+});
+
+const Div = glamorous.div({
+  width: '100%',
+});
+
+const H1 = glamorous.h1({
+  color: 'red',
+});
 
 const Navbar = () => (
-  <h1>Navbar</h1>
+  <Header>
+    <Nav>
+      <Div>
+        <Link href="/" to="/">
+          <H1>MXSDB</H1>
+        </Link>
+      </Div>
+    </Nav>
+  </Header>
 );
 
 export default Navbar;
