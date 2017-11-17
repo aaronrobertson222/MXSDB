d676923dfcccce1d5d23bbe0b6c216747056e901
aaronr5 <aaronr222@hotmail.com>
2017-11-17 02:50:59 -0500

    changes to landing nav

--- src/client/components/landing-navbar/landing-navbar.jsx
+++ src/client/components/landing-navbar/landing-navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import cssModules from 'react-css-modules';
 
 import styles from './landing-navbar.css';
@@ -9,15 +10,25 @@ import LogoSvg from '../../assets/images/MXSDB.svg';
 const LandingNavbar = () => (
   <div styleName="wrapper">
     <div styleName="container">
-      <LogoSvg styleName="logo" />
-      <div styleName="user-options">
-        <button>
-        Login
-        </button>
-        <button>
-        Signup
-        </button>
-      </div>
+      <Link to="/" href="/">
+        <LogoSvg styleName="logo" />
+      </Link>
+      <ul styleName="user-options">
+        <li>
+          <Link to="/app/login" href="/app/login" styleName="login-option">
+            <span>
+              Login
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/app/signup" href="/app/signup" styleName="signup-option">
+            <span>
+              Signup
+            </span>
+          </Link>
+        </li>
+      </ul>
     </div>
   </div>
 );
