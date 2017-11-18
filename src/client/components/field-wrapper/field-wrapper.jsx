8e5785dbb576e8ef12aeafff400cf318839b9593
aaronr5 <aaronr222@hotmail.com>
2017-11-18 02:11:35 -0500

    added field-wrapper component

---
+++ src/client/components/field-wrapper/field-wrapper.jsx
@@ -0,0 +1,23 @@
+// eslint-disable-file
+import React from 'react';
+import PropTypes from 'prop-types';
+import cssModules from 'react-css-modules';
+
+import styles from './field-wrapper.css';
+
+const FieldWrapper = props => (
+  <div styleName="field">
+    <label htmlFor={props.labelFor} styleName="label">
+      {props.label}
+    </label>
+    {props.children}
+  </div>
+);
+
+FieldWrapper.propTypes = {
+  label: PropTypes.string.isRequired,
+  labelFor: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+export default cssModules(FieldWrapper, styles);
